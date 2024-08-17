b5af77ac5b8ca5824e84c0310460deaa2789d49e
iqrazafarz98 <118602082+iqrazafarz98@users.noreply.github.com>
2024-08-17 19:01:31 +0000

    stack and heap

--- Basic/Conversion.js
+++ Basic/Conversion.js
@@ -3,3 +3,9 @@ let valueInNum =Number(score);
 console.log(typeof valueInNum )
 console.log(score);
 console.log(typeof score);
+//+++++++++++++++++++++++++++++
+//stack and heap
+//stack for primitive
+//heap for non pre
+// non primitive(refrense)
+//premitive(copy) 
