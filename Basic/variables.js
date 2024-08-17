2e7f5a9ed1fc7b16a2e628d414f9ab521e929ca7
iqrazafarz98 <118602082+iqrazafarz98@users.noreply.github.com>
2024-08-17 14:50:24 +0000

    variables

---
+++ Basic/variables.js
@@ -0,0 +1,14 @@
+"use strict"; //treat all ccode as newer version of js
+//alert(3+3)// we are not using browser ,its js so we dnt use it directly here
+let name="iqra"
+let age =26
+let IsLogIn= false
+
+//number 2=>52
+//bigInt
+//string
+//bool
+//null
+//undefined
+//symbol//use for uniqueness
+console.log(typeof age)
